835fa6d446ab8ba890c02b3641ca9f16c5139f14
Shaurya Pethe <rajesh.pethe+shaurya@gmail.com>
2022-11-15 12:19:49 +0530

    Completed assignment.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import login_user, login_required, UserMixin, LoginManager, logout_user
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -9,8 +11,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+app.secret_key = 'secret-key'
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.init_app(app)
 
-class Users(db.Model):
+
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(120))
@@ -32,14 +39,14 @@ def register():
         email = request.form.get('email', None)
         passwd = request.form.get('password', None)
         user = Users(name, email, passwd)
+
         # User model.save
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('login'))
-
-        
         # After user is saved, redirect to private page
+        return redirect(url_for('login'))
+        
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -50,11 +57,25 @@ def login():
         u = Users.query.filter_by(email=email).first()
 
         if u.passwd == passwd:
+            login_user(u)
             return redirect(url_for('private'))
+        else:
+            return render_template('error_login.html')
 
     return render_template('login.html')
 
-@app.route('/private')
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/private', methods=['GET'])
+@login_required
 def private():
     return render_template('private.html')
 
